e17ced73c10131f44f4acc15bee6091feff37629
Andrew <91447105+TriceratopsGang@users.noreply.github.com>
2026-01-27 23:51:52 -0500

    Unicode / updated to code standard

--- SimpleWindow/Main.cpp
+++ SimpleWindow/Main.cpp
@@ -1,23 +1,36 @@
 #include <windows.h>
+#include <stdio.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+    _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
 {
     const wchar_t CLASS_NAME[] = L"Simple Window Class";
 
-    WNDCLASS wc = {};
+    WNDCLASSEXW wc = {};
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszClassName = CLASS_NAME;
 
-    RegisterClass(&wc);
+    ATOM atom = RegisterClassExW(&wc);
+    if (atom == 0)
+    {
+        wchar_t buf[256];
+        swprintf_s(buf, L"RegisterClassExW failed (0x%08X)", GetLastError());
+        MessageBoxW(NULL, buf, L"Error", MB_OK | MB_ICONERROR);
+        return -1;
+    }
 
-    HWND hwnd = CreateWindowEx(
+    HWND hwnd = CreateWindowExW(
         0,
         CLASS_NAME,
-        L"Simple Win32 Window",
+        L"Simple Win32 Window",             
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
         NULL,
@@ -27,19 +40,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
     if (hwnd == NULL)
     {
-        return 0;
+        wchar_t buf[256];
+        swprintf_s(buf, L"CreateWindowExW failed (0x%08X)", GetLastError());
+        MessageBoxW(NULL, buf, L"Error", MB_OK | MB_ICONERROR);
+        return -1;
     }
 
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0))
+    int gm;
+    while ((gm = GetMessageW(&msg, NULL, 0, 0)) != 0)
     {
+        if (gm == -1)
+        {
+            // Failed
+            wchar_t buf[256];
+            swprintf_s(buf, L"GetMessage failed (0x%08X)", GetLastError());
+            MessageBoxW(NULL, buf, L"Error", MB_OK | MB_ICONERROR);
+            break;
+        }
+
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return 0;
+    return static_cast<int>(msg.wParam);
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -54,8 +81,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
+
+        if (hdc)
+        {
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(hwnd, &ps);
+        }
+        else
+        {
+            // Fallback
+        }
+
         return 0;
     }
 
